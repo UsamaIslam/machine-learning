b19c9ca6f95a8c6b74df495f059e31dc0bc59a2c
Muhammad Usama Bin Islam <usamaislam86@hotmail.com>
2018-11-30 13:40:11 +0500

    Update house_dataset.py

--- Assignment_1_KNN/house_dataset.py
+++ Assignment_1_KNN/house_dataset.py
@@ -1,26 +1,30 @@
-import Assignment_1.KNN_q as kn
+import Assignment_1_KNN.KNN_q as kn
 import pandas as pd
 import numpy as np
+
+
+def root_mean_square_error(actual, predictions):
+    return (((predictions - actual) ** 2).sum()) / len(predictions)
+
 
 test_df = pd.read_csv('data/kc_house_test_data.csv', usecols=[i for i in range(3, 21)], header=None, index_col=False)
 # print(test_df.values)
 test_df = list(test_df.values)
 # print(test_df[1::])
 
-test_df = test_df[1::]
+test_df = test_df[1:1000:]
 lable_test = pd.read_csv('data/kc_house_test_data.csv', usecols=[2, ], header=None, index_col=False)
 lable_test = list(lable_test.values)
-lable_test = lable_test[1::]
+lable_test = lable_test[1:1000:]
 train_df = pd.read_csv('data/kc_house_train_data.csv', usecols=[i for i in range(3, 21)], header=None, index_col=False)
 train_df = list(train_df.values)
-train_df = train_df[1::]
+train_df = train_df[1:2000:]
 lable_train = pd.read_csv('data/kc_house_train_data.csv', usecols=[2, ], header=None, index_col=False)
 lable_train = list(lable_train.values)
-lable_train = lable_train[1::]
+lable_train = lable_train[1:2000:]
 
 predictions = []
-neighbor = [kn.getNieghbors(train_df, testInstance, lable_train, k=5) for testInstance in test_df]
-predictions.append(kn.getResponse(neighbor))
-accuracy = kn.getAccuracy(lable_test, predictions)
-
-print(accuracy)
+for testInstance in test_df:
+    predictions.append(kn.getResponse(kn.getNeighbors(train_df, testInstance, lable_train, k=5)))
+rmse = root_mean_square_error(np.asarray(lable_test, dtype='float64'), np.asarray(predictions, dtype='float64'))
+print(rmse)
